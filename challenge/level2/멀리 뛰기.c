9519d6bac710176661b490be62f234f5da2031bd
eunwoo <lkjin0110@gmail.com>
2022-12-22 10:57:54 +0900

    멀리 뛰기

---
+++ challenge/level2/멀리 뛰기.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int arr[2001] = {0,};
+
+int DP(n)
+{
+    arr[1] = 1;
+    arr[2] = 2;
+    
+    for(int i = 3; i <= n; i++)
+        arr[i] = (arr[i - 2] + arr[i - 1]) % 1234567;
+    return arr[n];
+}
+
+long long solution(int n)
+{
+    return DP(n);
+}
