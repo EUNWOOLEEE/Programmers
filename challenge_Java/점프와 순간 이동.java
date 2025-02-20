0d7d741a945287b0630b88dbbb32e2f0d39a6030
eunwoo <lkjin0110@gmail.com>
2025-02-20 22:19:04 +0900

    2025.02.20_2

--- challenge_Java/점프와 순간 이동.java
+++ challenge_Java/점프와 순간 이동.java
@@ -2,10 +2,8 @@ public class Solution {
     public int solution(int n) {
         int answer = 0;
         while (n > 2) {
-            if (n % 2 != 0) {
+            if (n % 2 != 0)
                 answer++;
-                n--;
-            }
             n /= 2;
         }
         return answer + 1;
