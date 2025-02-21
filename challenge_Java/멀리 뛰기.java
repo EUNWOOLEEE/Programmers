f9fc26cd72e5e6d2f8ae7db2c59b636fd5c0bfc1
eunwoo <lkjin0110@gmail.com>
2025-02-21 12:52:59 +0900

    2025.02.21_1

---
+++ challenge_Java/멀리 뛰기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long solution(int n) {
+        if (n == 1) return 1;
+        
+        long[] jump = new long[n];
+        jump[0] = 1;
+        jump[1] = 2;
+        for (int i = 2; i < n; i++)
+            jump[i] = (jump[i - 1] + jump[i - 2]) % 1234567;
+        
+        return jump[n - 1];
+    }
+}
