3c41d4aa1111c5a9d76c3353e63fde36fb983283
eunwoo <lkjin0110@gmail.com>
2025-02-21 13:59:53 +0900

    구명보트 수정

--- challenge_Java/구명보트.java
+++ challenge_Java/구명보트.java
@@ -6,14 +6,28 @@ class Solution {
         int left = 0, right = people.length - 1;
         Arrays.sort(people);
         
-        for (int total = 0, cnt = 0; left < right; answer++) {
-            for (; cnt < 2 && right >= left && total + people[right] <= limit; cnt++)
-                total += people[right--];
-            for (; cnt < 2 && left < right && total + people[left] <= limit; cnt++)
-                total += people[left++];
-            total = cnt = 0;
+        while (left <= right) {
+            if (people[right] + people[left] <= limit) left++;
+            right--;
+            answer++;
         }
         
-        return right < left ? answer : answer + 1;
+        return answer;
     }
+
+    // public int solution(int[] people, int limit) {
+    //     int answer = 0;
+    //     int left = 0, right = people.length - 1;
+    //     Arrays.sort(people);
+        
+    //     for (int total = 0, cnt = 0; left < right; answer++) {
+    //         for (; cnt < 2 && right >= left && total + people[right] <= limit; cnt++)
+    //             total += people[right--];
+    //         for (; cnt < 2 && left < right && total + people[left] <= limit; cnt++)
+    //             total += people[left++];
+    //         total = cnt = 0;
+    //     }
+        
+    //     return right < left ? answer : answer + 1;
+    // }
 }
