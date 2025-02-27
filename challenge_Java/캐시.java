4ff14e97c29f7624b82944bf65051da8bed46277
eunwoo <lkjin0110@gmail.com>
2025-02-27 15:59:45 +0900

    캐시 수정

--- challenge_Java/캐시.java
+++ challenge_Java/캐시.java
@@ -2,17 +2,15 @@ import java.util.*;
 
 class Solution {
     public int solution(int cacheSize, String[] cities) {
-        HashMap<String, Integer> map = new HashMap<>();
+        HashSet<String> set = new HashSet<>();
         LinkedList<String> list = new LinkedList<>();
         int answer = 0;
         
         for (String city : cities) {
             city = city.toLowerCase();
             
-            if (map.containsKey(city) == false || map.get(city) < 1) {
-                map.put(city, 1);
+            if (set.add(city) == true)
                 answer += 5;
-            }
             else {
                 list.remove(city);
                 answer += 1;
@@ -21,7 +19,7 @@ class Solution {
             list.add(city);
             
             if (list.size() > cacheSize) {
-                map.put(list.get(0), map.get(list.get(0)) - 1);
+                set.remove(list.get(0));
                 list.remove(0);
             }
         }
